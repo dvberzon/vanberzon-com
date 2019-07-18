5c4a3a885feae5770e776c9e4ced9b899131d577
Daniel van Berzon <dvberzon@gmail.com>
2019-07-18 20:58:10 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-144169934-1`,
       },
     },
     `gatsby-plugin-feed`,
