30c371b1f5372a677c9cde8885b7d8dabdd30312
Daniel van Berzon <dvberzon@gmail.com>
2020-01-24 22:19:09 +0000

    adding riga talk and halfstack video

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -8,11 +8,17 @@ const TalksPage = ({location}) => (
     or meetups. Subjects range from code methodologies and readability to 
     specific technologies. Below are some examples of previous talks as well
     as any programmed future appearances.</p>
-    {/* <h2>Upcoming talks</h2> */}
+    <h2>Upcoming talks</h2>
+    <h3>Readable Code - Available without Prescription</h3>
+    <cite>Thu 2nd Apr 2020 - <a href="https://2020.frontcon.com/sessions/readable-code-available-without-prescription/">FrontCon, Riga, Latvia</a></cite>
     <h2>Recent talks</h2>
-    <cite>Fri 22nd Nov 2019 - <a href="https://halfstackconf.com/london/">HalfStack Conference London</a></cite>
+    <h3>Readable Code - Available without Prescription</h3>
+    <cite>Fri 22nd Nov 2019 - <a href="https://halfstackconf.com/london/2019/">HalfStack Conference London</a></cite>
     <p>Grammar is all about rules, but not all rules are the same. This talk tells the story of a revolution in the world of linguistics and how we can apply it to software development.</p>
     <p>With the help of Shakespeare, Noam Chomsky and some little green men, we will learn the dangers of prescriptive rules, and discover how an amateur online scientific experiment could help us write readable code.</p>
+    <p>
+      <a href="https://halfstackconf.streameventlive.com/archive/76">Watch the video <span role="img" aria-label="watch">👀</span></a>
+    </p>
     <p>Slides available on <a href="https://speakerdeck.com/dvberzon/readable-code-available-without-prescription">Speaker deck</a></p>
     <h3>The Nitty Gitty</h3>
     <cite>Thurs 5th Sep 2019 - <a href="https://asyncjs.com/nitty-gitty/">Async Brighton</a></cite>
