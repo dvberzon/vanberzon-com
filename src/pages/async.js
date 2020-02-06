70b82cd2856dd23ce2d7d7e6f827cc13044ca696
Daniel Berzon <daniel@ocasta.com>
2020-02-06 12:39:13 +0000

    async flyers

---
+++ src/pages/async.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Async = () => (
+  <>
+    <div style={{
+      backgroundColor: '#000',
+      textAlign: 'center',
+      minHeight: '100vh',
+    }}>
+      <img src="./flyer.png" style={{ maxHeight: '100vh'}}/>
+    </div>
+    <div style={{
+      backgroundColor: '#ffde00',
+      textAlign: 'center',
+      minHeight: '100vh',
+    }}>
+      <img src="./discount.png" />
+    </div>
+  </>
+);
+
+export default Async;
