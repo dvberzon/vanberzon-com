021cb98feeb7aa7557bcc72641b52d23c2090fb8
Daniel van Berzon <dvberzon@gmail.com>
2020-07-17 21:23:16 +0100

    adding nitty gitty online talk

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -12,6 +12,12 @@ const TalksPage = ({location}) => (
     <h3>Readable Code - Available without Prescription</h3>
     <cite>Date TBC - <a href="https://2020.frontcon.com/sessions/readable-code-available-without-prescription/">FrontCon, Riga, Latvia</a></cite>
     <h2>Recent talks</h2>
+    <h3>The Nitty Gitty - demystifying git</h3>
+    <cite>Wed 13th May 2020 - <a href="https://www.meetup.com/MEGABITES-Tech-Lunch-n-Learns/events/270359119/">Megabites Tech Lunch and Learns</a> (online)</cite>
+    <p>People treat Git like it's a car, but really it's more like a skateboard. In this talk I attempt to demystify git by looking at how  Git works under the hood. We look at the basics of Git, its building blocks, its branch model and how to manipulate its history.</p>
+    <p>This talk was presented online as part of a series of <a href="https://www.meetup.com/MEGABITES-Tech-Lunch-n-Learns/">lunchtime tech talks</a></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/w7W8xVpQt9M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p>Slides available on <a href="https://speakerdeck.com/dvberzon/the-nitty-gitty-a8f1454b-133e-44a6-b6a6-e6ba2ff2bef4">Speaker deck</a></p>
     <h3>Readable Code - Available without Prescription</h3>
     <cite>Fri 22nd Nov 2019 - <a href="https://halfstackconf.com/london/2019/">HalfStack Conference London</a></cite>
     <p>Grammar is all about rules, but not all rules are the same. This talk tells the story of a revolution in the world of linguistics and how we can apply it to software development.</p>
